1ade332db404b3e3bdc1ad6549fcb595edb51e9d
Ashray4 <skspace12@gmail.com>
2025-08-29 20:32:51 +0200

    removed depreciation warnings

--- cartesian_controller_handles/src/motion_control_handle.cpp
+++ cartesian_controller_handles/src/motion_control_handle.cpp
@@ -312,8 +312,12 @@ geometry_msgs::msg::PoseStamped MotionControlHandle::getEndEffectorPose()
 {
   KDL::JntArray positions(m_joint_handles.size());
   for (size_t i = 0; i < m_joint_handles.size(); ++i)
-  {
-    positions(i) = m_joint_handles[i].get().get_value();
+  { 
+    #if defined CARTESIAN_CONTROLLERS_JAZZY
+      positions(i) = m_joint_handles[i].get().get_optional().value();
+    #else
+      positions(i) = m_joint_handles[i].get().get_value();
+    #endif
   }
 
   KDL::Frame tmp;
